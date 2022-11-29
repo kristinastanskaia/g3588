51c02fd62c4aae7f597403200ce35bd4777c2e6e
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-11-29 22:02:51 +0300

    add Seminar1Task7

---
+++ Seminar1Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+int inputNumber = int.Parse(inputLine);
+
+int lastDigit = inputNumber % 10;
+
+
+Console.WriteLine(lastDigit);
+}
