8cdfa3c6ff86746533b682a819a9a8593e47516a
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-11-29 21:49:43 +0300

    add SemTask0

---
+++ SemTask0/Program.cs
@@ -0,0 +1,10 @@
+﻿string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+if(num1Line!=null&&num2Line!=null)
+{
+int num1 = int.Parse(num1Line);
+int num2 = int.Parse(num2Line);
+int outResult = num1*num2;
+Console.WriteLine(outResult);
+}
