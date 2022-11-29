e1d05a3c3185c07b430ef6a10592de9179b541ee
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-11-29 22:02:15 +0300

    add Seminar1Task5

--- Seminar1Task5/Program.cs
+++ Seminar1Task5/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(inputLine!=null)
+{
+
+   //Парсим введенное число
+   int inputNumber = int.Parse(inputLine);
+
+   int startNumber = inputNumber*(-1);
+
+   //Выходное значение
+   string outLine = string.Empty;
+
+   while(startNumber<inputNumber)
+   {
+      outLine = outLine + startNumber + ", ";
+      startNumber++;
+      //startNumber=startNumber+1;
+   }
+   outLine = outLine + inputNumber;
+
+   // Выводим данные в консоль 
+   Console.WriteLine(outLine);
+}
