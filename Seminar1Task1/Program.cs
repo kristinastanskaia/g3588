2e0ed0a3af0c0c197d3b0f68252f4e7b6f694e50
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-11-29 21:50:16 +0300

    add Seminar1Task1

---
+++ Seminar1Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+if (num1Line != null && num2Line != null)
+{
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+   
+    if (num2 * num2 == num1)
+    {
+        Console.WriteLine("Первое число квадрат второго");
+    }
+    else
+    {
+        Console.WriteLine("Первое число НЕ квадрат второго");
+    }
+}
