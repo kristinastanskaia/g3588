f79af2ae13c90ecdf3282cc524ae2e81857c7359
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-02 23:06:00 +0300

    added changes Seminar1Task8

---
+++ Seminar1Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int startNumber = 2;
+    string outLine = string.Empty;
+    int chek = inputNumber % 2;
+
+    while (startNumber < inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber = startNumber + 2;
+    }
+    if (chek == 0)
+    {
+        outLine = outLine + inputNumber;
+    }
+    Console.WriteLine(outLine);
+}
