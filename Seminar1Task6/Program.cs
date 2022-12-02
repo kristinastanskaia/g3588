99d937b1723be4e8f9b97b4ad3f5f8a1cf2874dd
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-02 23:05:23 +0300

    added changes Seminar1Task6

---
+++ Seminar1Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿string? num1Line = Console.ReadLine();
+
+if (num1Line != null)
+{
+    int num1 = int.Parse(num1Line);
+    int check = num1 % 2;
+
+    if (check == 0)
+    {
+        Console.WriteLine("Четное число");
+    }
+    else
+    {
+        Console.WriteLine("Нечетное число");
+    }
+}
+
