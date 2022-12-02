7dd5c334b83a3357eb72df1e12daf3f0c0a8a6ea
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-02 23:07:06 +0300

    added changes Seminar1Task2

--- Seminar1Task2/Program.cs
+++ Seminar1Task2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+if (num1Line != null && num2Line != null)
+{
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    if (num1 > num2)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+}
