113b332c0f308dcbe3a08511e6503c22b659c92b
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-04 13:59:47 +0300

    added comments to the solution

--- Seminar1Task2/Program.cs
+++ Seminar1Task2/Program.cs
@@ -1,17 +1,24 @@
-﻿string? num1Line = Console.ReadLine();
+﻿string? num1Line = Console.ReadLine();  // Считываем данные с консоли
 string? num2Line = Console.ReadLine();
 
-if (num1Line != null && num2Line != null)
+if (num1Line != null && num2Line != null)   // Проверяем, чтобы данные были не пустыми
 {
-    int num1 = int.Parse(num1Line);
+    int num1 = int.Parse(num1Line); // Парсим введеное число
     int num2 = int.Parse(num2Line);
 
-    if (num1 > num2)
+    if (num1 == num2)   // Проверяем, равны ли числа?
     {
-        Console.WriteLine("Первое число больше второго");
+        Console.WriteLine("Числа равны");
     }
     else
     {
-        Console.WriteLine("Второе число больше первого");
+        if (num1 > num2)    // Проверяем, первое число больше второго?
+        {
+            Console.WriteLine("Первое число больше второго");
+        }
+        else
+        {
+            Console.WriteLine("Второе число больше первого");
+        }
     }
 }
