b463edcafd4cd86660632fbce758cc6ef44aba2f
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-04 15:22:34 +0300

    added comments to the solution

--- Seminar1Task8/Program.cs
+++ Seminar1Task8/Program.cs
@@ -1,20 +1,22 @@
-﻿string? inputLine = Console.ReadLine();
+﻿string? inputLine = Console.ReadLine();     // Считываем данные с консоли
 
-if (inputLine != null)
+if (inputLine != null)      // Проверяем, чтобы данные были не пустыми
 {
-    int inputNumber = int.Parse(inputLine);
-    int startNumber = 2;
-    string outLine = string.Empty;
-    int chek = inputNumber % 2;
+    int inputNumber = int.Parse(inputLine);     // Парсим введеное число
+    int startNumber = 2;        // Вводим переменную, с которой начинается ряд четных чисел
+    string outLine = string.Empty;      // Вводим переменную, которая накапливает ответ
+    int chek = inputNumber % 2;     // Вводим переменную, равную остатку от деления на 2
 
-    while (startNumber < inputNumber)
+    while (startNumber < inputNumber)       // Вводим условие цикла
     {
-        outLine = outLine + startNumber + ", ";
+        outLine = outLine + startNumber + ",";     // Накапливаем ответ
         startNumber = startNumber + 2;
     }
-    if (chek == 0)
+
+    if (chek == 0)      //Проверяем, остаток от деления равен 0 (четное ли введеное число?)
     {
-        outLine = outLine + inputNumber;
+        outLine = outLine + inputNumber;        // Добавляем к ответу и введеное число
     }
+    outLine = outLine.TrimEnd(',');            //Удаляем крайнюю запятую
     Console.WriteLine(outLine);
 }
