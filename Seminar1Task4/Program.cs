f24fbef79ba3fe05de57727d14f81275007d2395
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-04 14:07:29 +0300

    added comments to the solution

--- Seminar1Task4/Program.cs
+++ Seminar1Task4/Program.cs
@@ -1,16 +1,16 @@
-﻿string? num1Line = Console.ReadLine();
+﻿string? num1Line = Console.ReadLine();  // Считываем данные с консоли
 string? num2Line = Console.ReadLine();
 string? num3Line = Console.ReadLine();
 
-if (num1Line != null && num2Line != null && num3Line != null)
+if (num1Line != null && num2Line != null && num3Line != null)   // Проверяем, чтобы данные были не пустыми
 {
-    int num1 = int.Parse(num1Line);
+    int num1 = int.Parse(num1Line); // Парсим введеное число
     int num2 = int.Parse(num2Line);
     int num3 = int.Parse(num3Line);
 
-    if (num1 > num2)
+    if (num1 > num2)    // Проверяем, первое число больше второго?
     {
-        if (num1 > num3)
+        if (num1 > num3) // Проверяем, первое число больше третьего?
         {
             Console.WriteLine("max " + num1);
         }
@@ -21,7 +21,7 @@ if (num1Line != null && num2Line != null && num3Line != null)
     }
     else
     {
-        if (num2 < num3)
+        if (num2 < num3)    // Проверяем, второе число меньше третьего?
         {
             Console.WriteLine("max " + num3);
         }
