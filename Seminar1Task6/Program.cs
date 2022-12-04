5aca5ec2e7a76baf2b288e85582ccaa37e2e34d5
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-04 14:16:47 +0300

    added comments to the solution

--- Seminar1Task6/Program.cs
+++ Seminar1Task6/Program.cs
@@ -1,11 +1,11 @@
-﻿string? num1Line = Console.ReadLine();
+﻿string? num1Line = Console.ReadLine();       // Считываем данные с консоли
 
-if (num1Line != null)
+if (num1Line != null)     // Проверяем, чтобы данные были не пустыми
 {
-    int num1 = int.Parse(num1Line);
-    int check = num1 % 2;
+    int num1 = int.Parse(num1Line);     // Парсим введеное число
+    int check = num1 % 2;       // Находим остаток от деления на 2
 
-    if (check == 0)
+    if (check == 0)      // Проверям, равен ли остаток от делению нулю
     {
         Console.WriteLine("Четное число");
     }
