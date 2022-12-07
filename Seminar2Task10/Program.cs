fb2b40f94a8ebf3449871ae01d05167fc9ce3ff0
KristinaStanskaia <kristina.stanskaia@gmail.com>
2022-12-07 16:52:11 +0300

    added new tasks

---
+++ Seminar2Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number < 1000 && number > 99)
+{
+
+    string value = number.ToString();
+
+    char[] numberArray = value.ToCharArray();
+
+    Console.WriteLine(numberArray[1] + " - вторая цифра этого числа");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+
---
+++ Seminar2Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// //ВАРИАНТ 1
+// int num = int.Parse(Console.ReadLine() ?? "0");
+
+// if (num > 0 && num < 8)
+// {
+//     if (num < 6)
+//     {
+//         Console.WriteLine("Это рабочий день");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Это выходной день");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Это не день недели!");
+// }
+
+//ВАРИАНТ 2
+
+int num = int.Parse(Console.ReadLine() ?? "0");
+if (num > 0 && num < 8)
+{
+    string[] week = new string[7];
+
+    week[0] = "рабочий день";
+    week[1] = "рабочий день";
+    week[2] = "рабочий день";
+    week[3] = "рабочий день";
+    week[4] = "рабочий день";
+    week[5] = "выходной день";
+    week[6] = "выходной день";
+    Console.WriteLine(week[num - 1]);
+}
+else
+{
+    Console.WriteLine("Это не день недели!");
+}
+
+
